74648cb6cbc4bc73906ab36acf477a951fde7b16
hankrum <st_kolev@mail.bg>
2017-05-23 08:01:59 +0300

    Migration fixed

--- RestaurantSystem/RestaurantSystem.ConsoleTestGround/Engine.cs
+++ RestaurantSystem/RestaurantSystem.ConsoleTestGround/Engine.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity.Migrations;
+using System.Data.Entity;
+using RestaurantSystem.Data.Migrations;
 
 namespace RestaurantSystem.ConsoleTestGround
 {
@@ -15,6 +18,11 @@ namespace RestaurantSystem.ConsoleTestGround
 
             var db = new RestaurantSystemDbContext();
 
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion
+                    <RestaurantSystemDbContext, Configuration>());
+
+            db.SaveChanges();
             Console.WriteLine(db.Address.ToList().Count);
         }
     }
