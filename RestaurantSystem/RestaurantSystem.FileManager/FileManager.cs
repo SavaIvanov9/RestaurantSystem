d592fdda93cb089c23d0da53b49c79736306f4ff
Savata <sava.ivanov.9@gmail.com>
2017-06-10 16:27:18 +0300

    fix

--- RestaurantSystem/RestaurantSystem.FileManager/FileManager.cs
+++ RestaurantSystem/RestaurantSystem.FileManager/FileManager.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RestaurantSystem.FileManager
+﻿namespace RestaurantSystem.FileManager
 {
+    using System;
+    using System.IO;
+
     public class FileManager : IFileManager
     {
         public byte[] ReadFile(string path)
--- RestaurantSystem/RestaurantSystem.FileManager/IFileManager.cs
+++ RestaurantSystem/RestaurantSystem.FileManager/IFileManager.cs
@@ -1,9 +1,12 @@
-﻿namespace RestaurantSystem.FileManager
+﻿
+namespace RestaurantSystem.FileManager
 {
+    using System;
+
     public interface IFileManager
     {
         byte[] ReadFile(string path);
 
-        Tuple<bool, string> WriteFile(byte[] file, string directory, string fileName)
+        Tuple<bool, string> WriteFile(byte[] file, string directory, string fileName);
     }
 }
